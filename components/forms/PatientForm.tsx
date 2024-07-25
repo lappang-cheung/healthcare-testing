f47f80b6a5d952f527d6030007b9920e3bf5141b
Leo Cheung <lappangcheung@gmail.com>
2024-07-25 18:20:05 -0400

    Remove the console log

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -43,7 +43,6 @@ const PatientForm = () => {
       const userData = { name, email, phone };
       const user = await createUser(userData)
 
-      console.log('user', user)
       if (user) router.push(`/patients/${user.$id}/register`)
 
     }catch (error) {
